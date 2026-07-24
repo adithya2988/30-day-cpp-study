c01ef780912afdd59f207b589091fee70b228f35
adithya <adithyas2988@gmail.com>
2026-07-24 21:29:50 +0530

    areaofcircle

---
+++ code w harry/areaofcircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float radius, area;
+    cout<<"enter the radius of the circle= ";
+    cin>>radius;
+    area=3.14*radius*radius;
+    cout<<"area of the cicle is= "<<area;
+    return 0;
+}
