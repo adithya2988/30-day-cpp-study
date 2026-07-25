487d577639e168ea7242f5d120aa57031cb6fa74
adithya <adithyas2988@gmail.com>
2026-07-25 07:08:56 +0530

    Renamed folder from code w harry to cpp problems

---
+++ cpp problems/sumoftwonumbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+    cout << "enter two numbers: ";
+    cin >> num1>>num2;
+    sum=num1+num2;
+    cout << " sum is equal to: "<<sum;
+    return 0;
+
+}
