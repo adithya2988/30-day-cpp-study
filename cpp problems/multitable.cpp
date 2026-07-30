a4858b0d5a705bc63fda578eda961a5298d37a9d
adithya <adithyas2988@gmail.com>
2026-07-30 12:07:18 +0530

    multitable

---
+++ cpp problems/multitable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter the number: ";
+    cin>>n;
+    cout<<"multiplication table of  "<<n<<" is: "<<endl;
+    for (int i=1;i<=10;i++)
+    {
+        cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+    }
+    return 0;
+}
