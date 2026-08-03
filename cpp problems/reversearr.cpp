d99a6b3651b44598d4e9ea3a99b002939059ea94
adithya <adithyas2988@gmail.com>
2026-08-03 20:13:54 +0530

    reversearr

--- cpp problems/reversearr.cpp
+++ cpp problems/reversearr.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main()                 {
- int arr[]={10,20,30,40,50};
-                          for(int i=4;i>=0;i--)
+int main() {
+    int arr[]={10,20,30,40,50};
+    for(int i=4;i>=0;i--)
     {
-cout<< arr[i]<<" ";                
+        cout<< arr[i]<<" ";                
     }
     
     return 0;
